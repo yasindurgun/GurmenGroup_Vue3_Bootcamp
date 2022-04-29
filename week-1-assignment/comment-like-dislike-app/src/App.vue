f5a89703fd43b9aa5662bd970cdb29a2d17138ca
yasindurgun <yasin.durgun52@gmail.com>
2022-04-29 06:26:11 +0300

    comment like dislike app done

---
+++ week-1-assignment/comment-like-dislike-app/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+ <header>Comment Like/Dislike App</header>
+    <div class="container">
+      <div class="comment--like--dislike--app">
+
+        <CommentItem :like="like" :dislike="dislike" v-for="comment in provideData.comments" :key="comment.id" :comment="comment"/>
+
+
+      </div>
+    </div>
+</template>
+
+<script>
+import CommentItem from "@/components/CommentItem"
+export default {
+components : {
+    CommentItem
+  },
+  data(){
+    return {
+      provideData: {
+      comments: [
+        {id: 1, content: "Güzel şehir.", like: 0, dislike: 0},
+        {id: 2, content: "Muazzam tatlı", like: 0, dislike: 0},
+        {id: 3, content: "Bozuk ürün", like: 0, dislike: 0},
+      ]
+      }
+    };
+  },
+  provide(){
+    return {
+      provideData: this.provideData.comments
+    }
+  },
+  // computed : {
+  //        like() {
+  //            console.log("deneme");
+  //            return this.provideData.comments.like;
+  //        },
+  //        dislike(){
+  //            return this.provideData.comments.dislike;
+  //        }
+  //    }
+}
+</script>
---
+++ week-1-assignment/comment-like-dislike-app/src/components/CommentItem.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="card mb-10 comment--item">
+          <p>
+             {{comment.content}} 
+          </p>
+          <div class="action--button--container text-right mt-10">
+            <!-- <button @click="comment.like++" class="btn-sm btn-success" :id="comment">Beğendim ({{comment.like}})</button> -->
+            <button @click="likeComment(comment)" class="btn-sm btn-success" :id="comment" :key="comment">Beğendim ({{ comment.like }})</button>
+            <button @click="dislikeComment(comment)" class="btn-sm btn-danger" :id="comment" :key="comment">Beğenmedim ({{ comment.dislike }})</button>
+          </div>
+        </div>
+</template>
+
+<script>
+ export default {
+    //  props : ["comment", "like","dislike"],
+     props : ["comment"],
+     inject: ["provideData"],
+    methods: {
+        likeComment(comment){
+            //console.log(comment.like)
+            comment.like++;
+            //console.log("like")
+        },
+        dislikeComment(comment){
+            //console.log(comment.dislike)
+            comment.dislike++;
+            //console.log("dislike")
+        }
+    }
+ }
+</script>
