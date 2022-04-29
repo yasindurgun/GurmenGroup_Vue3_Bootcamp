76efa9dd7f4f47cf35770feddca3128ce206d227
yasindurgun <yasin.durgun52@gmail.com>
2022-04-29 05:22:17 +0300

    bookmark app done

---
+++ week-1-assignment/bookmark-app/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+<header>Bookmark App</header>
+
+    <div class="container">
+      <div class="bookmark--app--container">
+        
+        <BookmarkItem v-for="bookmark in provideData.BookmarkItems" :key="bookmark.id" :item="bookmark"/>
+
+      </div>
+    </div>
+</template>
+
+<script>
+import BookmarkItem from "@/components/BookmarkItem"
+
+export default {
+  data(){
+    return {
+      provideData : {
+        BookmarkItems: [
+        {id: 1, title: "Google", description: "Açıklama 1", link:"https://www.google.com/", clicked:false},
+        {id: 2, title: "Facebook", description: "Açıklama 2", link:"https://www.facebook.com/", clicked:false},
+        {id: 3, title: "Linkedin", description: "Açıklama 3", link:"https://www.linkedin.com/", clicked:false},
+        {id: 4, title: "Instagram", description: "Açıklama 4", link:"https://www.instagram.com/", clicked:false},
+        {id: 5, title: "Twitch", description: "Açıklama 5", link:"https://www.twitch.tv/", clicked:false}
+      ]
+      }
+    };
+  },
+  components: {
+        BookmarkItem
+    },
+
+    provide() {
+      return {
+        provideData: this.provideData,
+        deleteBookmark: this.deleteBookmark
+      }
+    },
+
+    methods: {
+      deleteBookmark(bookmark){
+        this.provideData.BookmarkItems = this.provideData.BookmarkItems.filter((b) => b != bookmark);
+      }
+    }
+}
+</script>
---
+++ week-1-assignment/bookmark-app/src/components/BookmarkItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="bookmark-item">
+          <div class="header">
+            <h3>{{item.title}}</h3>
+            <button @click="deleteBookmark(item)" class="btn-sm btn-danger">Kaydı Sil</button>
+          </div>
+          <p class="mt-20 mb-20">{{item.description}}</p>
+          <button @click="gotoWebSite(item)" class="btn-primary btn-sm" id="item.id">Göster</button>
+        </div>
+</template>
+
+<script>
+export default {
+    data(){
+    },
+    methods: {
+        gotoWebSite(item){
+            //alert();
+            // window.open("https://www.google.com/","_blank");
+            window.open(item.link,"_blank");
+        }
+    },
+    props : ["item"],
+    inject : ["provideData", "deleteBookmark"]
+}
+</script>
